630e0517dbe9e0070e59a15b6734beb4d08c757b
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2021-05-11 16:04:36 -0300

    Fix ReposMapper test

--- app/src/test/java/br/com/concrete/ghpulls/CheckKoinModulesTest.kt
+++ app/src/test/java/br/com/concrete/ghpulls/CheckKoinModulesTest.kt
@@ -1,13 +1,20 @@
 package br.com.concrete.ghpulls
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 import org.koin.test.KoinTest
 import org.koin.test.check.checkModules
 
-class CheckKoinModulesTest : KoinTest {
+@RunWith(AndroidJUnit4::class)
+class CheckKoinModulesTest {
     @Test
-    fun checkAllKoinModules() = checkModules {
-        modules(koinModules("https://example.com", false))
+    fun checkAllKoinModules() {
+        checkModules {
+            modules(koinModules("https://example.com", false))
+        }
     }
 }
