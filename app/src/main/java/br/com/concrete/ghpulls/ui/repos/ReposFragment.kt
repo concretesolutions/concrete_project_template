11c0e3b874c2f4fcc40099219dd9e429620685a9
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2021-05-11 14:38:12 -0300

    Add first load/error state handling

--- app/src/main/java/br/com/concrete/ghpulls/ui/repos/ReposFragment.kt
+++ app/src/main/java/br/com/concrete/ghpulls/ui/repos/ReposFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import br.com.concrete.ghpulls.R
 import br.com.concrete.ghpulls.databinding.FragmentReposBinding
 import br.com.concrete.ghpulls.util.ItemVerticalSpaceDecorator
@@ -45,10 +47,27 @@ class ReposFragment : Fragment() {
             ItemVerticalSpaceDecorator(R.dimen.default_margin)
         )
 
+        binding.errorTryAgainButton.setOnClickListener {
+            reposAdapter.retry()
+        }
+
         lifecycleScope.launch {
             reposViewModel.kotlinReposPager.collectLatest {
                 reposAdapter.submitData(it)
             }
+        }
+
+        lifecycleScope.launch {
+            reposAdapter.loadStateFlow
+                .collectLatest { loadState ->
+                    if (loadState.refresh is LoadState.Loading) {
+                        binding.progress.show()
+                    } else {
+                        binding.progress.hide()
+                    }
+
+                    binding.errorGroup.isVisible = loadState.refresh is LoadState.Error
+                }
         }
     }
 }
