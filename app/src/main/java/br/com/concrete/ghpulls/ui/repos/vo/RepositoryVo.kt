7f1212c7bff84fdd830e4fbf70b7f06ae9e61d48
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2021-05-21 10:58:39 -0300

    Fix some detekt issues

--- app/src/main/java/br/com/concrete/ghpulls/ui/repos/vo/RepositoryVo.kt
+++ app/src/main/java/br/com/concrete/ghpulls/ui/repos/vo/RepositoryVo.kt
@@ -26,4 +26,3 @@ sealed class RepoBaseVo {
         }
     }
 }
-
--- app/src/main/java/br/com/concrete/ghpulls/ui/search/DashboardFragment.kt
+++ app/src/main/java/br/com/concrete/ghpulls/ui/search/DashboardFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import br.com.concrete.ghpulls.databinding.FragmentDashboardBinding
 
