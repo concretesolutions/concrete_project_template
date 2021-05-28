90cac9f0992cca2bee5bbae8215f92f31b98acd7
Cristiano Cortez <cristiano.p.cortez@concrete.com.br>
2021-05-28 15:15:00 -0300

    Correção de teste de injeção com o Koin

--- app/src/test/java/br/com/concrete/ghpulls/CheckKoinModulesTest.kt
+++ app/src/test/java/br/com/concrete/ghpulls/CheckKoinModulesTest.kt
@@ -1,6 +1,9 @@
 package br.com.concrete.ghpulls
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.android.ext.koin.androidContext
@@ -12,10 +15,17 @@ import org.koin.test.check.checkModules
 
 @RunWith(AndroidJUnit4::class)
 class CheckKoinModulesTest : AutoCloseKoinTest() {
+    private lateinit var context: Context
+
+    @Before
+    fun getContext() {
+       context = ApplicationProvider.getApplicationContext()
+    }
+
     @Test
     fun checkAllKoinModules() {
         checkModules {
-            modules(koinModules("https://example.com", false))
+            modules(koinModules("https://example.com", false, context))
         }
     }
 }
